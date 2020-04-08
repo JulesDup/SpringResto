8b41cd7bc9ae64f1a7b767c0f8d297a3849d2706
Jules Dupuis <jules.dupuis51@gmail.com>
2020-04-08 17:13:42 +0200

    first commit

--- src/main/java/dev/config/AppConfig.java
+++ src/main/java/dev/config/AppConfig.java
@@ -1,10 +1,7 @@
 package dev.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ComponentScan;
-
-@Configuration
-@ComponentScan
 public class AppConfig {
 
 }
+
+
