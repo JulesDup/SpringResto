fcb8e64369e8c8d135f8128fe4e7d23589d30f42
Jules Dupuis <jules.dupuis51@gmail.com>
2020-04-08 17:13:21 +0200

    first commit

---
+++ src/main/java/dev/AppJavaSpring.java
@@ -0,0 +1,33 @@
+package dev;
+
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dev.config.AppConfig;
+import dev.ihm.Menu;
+
+public class AppJavaSpring {
+
+	public static void main(String[] args) {
+		// Création du contexte Spring
+		// ClassPathXmlApplicationContext context = new
+		// ClassPathXmlApplicationContext("application-config-memoire.xml");
+
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		// récupération du bean Menu
+		Scanner scanner = context.getBean(Scanner.class);
+		Menu menu = context.getBean(Menu.class);
+
+		menu.afficher();
+
+		// fermeture du Scanner
+		context.getBean(Scanner.class).close();
+		// fermeture du contexte Spring
+		context.close();
+
+	}
+
+}
---
+++ src/main/java/dev/config/AppConfig.java
@@ -0,0 +1,10 @@
+package dev.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+}
