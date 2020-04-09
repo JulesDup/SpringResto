c85fa6d00a34caa64baf0f5e74b5245a30b1aa00
Jules Dupuis <jules.dupuis51@gmail.com>
2020-04-09 14:31:50 +0200

    tp correction 1.2

--- src/main/java/dev/ihm/Menu.java
+++ src/main/java/dev/ihm/Menu.java
@@ -8,6 +8,7 @@ import dev.ihm.options.OptionTerminer;
 import dev.service.IPlatService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -20,12 +21,29 @@ public class Menu {
 
 	private String menu;
 	private Scanner scanner;
+//
+//	public Menu(Scanner scanner, IPlatService service) {
+//		actions.put(1, new OptionListerPlats(service));
+//		actions.put(2, new OptionAjouterPlat(scanner, service));
+//		actions.put(99, new OptionTerminer());
+//		this.scanner = scanner;
+//	}
 
-	public Menu(Scanner scanner, IPlatService service) {
-		actions.put(1, new OptionListerPlats(service));
-		actions.put(2, new OptionAjouterPlat(scanner, service));
-		actions.put(99, new OptionTerminer());
+	public Menu(Scanner scanner, List<IOptionMenu> options) {
 		this.scanner = scanner;
+		this.actions = buildActions(options);
+	}
+
+	public Map<Integer, IOptionMenu> buildActions(List<IOptionMenu> options) {
+		Map<Integer, IOptionMenu> actions = new HashMap<Integer, IOptionMenu>();
+		int compteur = 1;
+		for (IOptionMenu opt : options) {
+
+			actions.put(compteur, opt);
+			compteur++;
+		}
+
+		return actions;
 	}
 
 	public void afficher() {
