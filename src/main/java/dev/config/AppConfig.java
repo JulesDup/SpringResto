98ed914cbcec5dd619246a026a153ba6af94f57e
Jules Dupuis <jules.dupuis51@gmail.com>
2020-04-10 18:07:46 +0200

    implémentation ajouter plat

--- src/main/java/dev/config/AppConfig.java
+++ src/main/java/dev/config/AppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @ComponentScan({ "dev.ihm", "dev.service", "dev.dao" })
 @PropertySource("app.properties")
-@Import(DataSourceConfig.class) // peut ettre remplacé ac cOMPOENENT
+@Import(DataSourceConfig.class) // peut ettre remplacé ac COMPONENENT
 public class AppConfig {
 
 	@Bean
